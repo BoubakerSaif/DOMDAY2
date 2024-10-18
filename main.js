8a280555aa4f5c39bcf15bbe152f1cc48ccdeeac
BoubakerSaif <boubakersaif.contact@gmail.com>
2024-10-18 20:37:51 +0100

    dom

---
+++ main.js
@@ -0,0 +1,72 @@
+/*
+Create Element
+createAttribute
+appendChild
+*/
+
+// let myElement = document.createElement("div");
+// let myAttr = document.createAttribute("data_custom");
+// let myText = document.createTextNode("Product One");
+
+// myElement.className = "product";
+// myElement.setAttributeNode(myAttr);
+// myElement.setAttribute("data_testing", "test");
+
+// // Appending Text to Element
+// myElement.appendChild(myText);
+
+// // Appending Element to Body
+
+// document.body.appendChild(myElement);
+
+/*
+Children
+childNodes
+firstChild
+lastChild
+firstElementChild
+lastElementChild
+
+*/
+
+// let myElement = document.querySelector("div");
+// console.log(myElement);
+// console.log(myElement.children);
+// console.log(myElement.childNodes);
+// console.log(myElement.firstChild);
+// console.log(myElement.lastChild);
+// console.log(myElement.firstElementChild);
+// console.log(myElement.lastElementChild);
+
+/*Event */
+
+let myBtn = document.querySelector("#btn");
+let minusBtn = document.querySelector("#minus");
+// myBtn.addEventListener("click", function (params) {});
+
+// myBtn.onclick = function () {
+//   let myP = document.querySelector("p");
+
+//   myP.innerHTML = Number(myP.innerHTML) + 1;
+// };
+
+// minusBtn.onclick = function () {
+//   let myP = document.querySelector("p");
+//   if (Number(myP.innerHTML) > 0) {
+//     myP.innerHTML = Number(myP.innerHTML) - 1;
+//   }
+// };
+
+// myBtn.oncontextmenu = function () {
+//   console.log("Clicked");
+// };
+
+// let myDiv = document.querySelector("div");
+// console.log(myDiv);
+
+// myBtn.onmouseenter = function () {
+//   myDiv.setAttribute("style", "display:block");
+// };
+// myBtn.onmouseleave = function () {
+//   myDiv.setAttribute("style", "display:none");
+// };
